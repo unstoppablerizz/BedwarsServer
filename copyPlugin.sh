c983b82ea7231dac147788f171822d4e7a44aa49
KirKirKsd <sapetp54@gmail.com>
2024-04-15 12:01:15 +0300

    Init git

---
+++ copyPlugin.sh
@@ -0,0 +1,1 @@
+cp "/mnt/f/KirKirKsd/Java/BedwarsMinecraft/target/BedwarsMinecraft-1.0.jar" "plugins/"
---
+++ start.sh
@@ -0,0 +1,3 @@
+cp "/mnt/f/KirKirKsd/Java/BedwarsMinecraft/target/BedwarsMinecraft-1.0.jar" "plugins/"
+java.exe -Xmx1024M -Xms1024M -jar spigot-1.20.4.jar nogui
+
